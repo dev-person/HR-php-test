c20b8099265280b3a99e7351b949c2bcecd08344
Djockey <admin@currency-trade.ru>
2018-11-09 21:36:27 +0500

    twig install and generate auth page

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/weather', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/welcome', 'HomeController@index')->name('home');
